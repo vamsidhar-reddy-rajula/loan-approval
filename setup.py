dc4e5b8b9e2ed7644e47eb7eba33791b7fe86230
vamsidhar Reddy Rajula <vamsidhar_reddy_rajula@protonmail.com>
2021-08-05 22:58:32 -0700

    modified setup.py

--- setup.py
+++ setup.py
@@ -7,4 +7,18 @@ setup(
     description='Predict whether a loan will be approved or not',
     author='Vamsidhar',
     license='MIT',
+    install_requires=[
+        'seaborn==0.11.1',
+        'matplotlib==3.4.2',
+        'plotly==5.1.0',
+        'statsmodels==0.12.2',
+        'click==7.1.2',
+        'joblib==1.0.1',
+        'Flask_RESTful==0.3.9',
+        'Flask==2.0.1',
+        'numpy==1.21.0',
+        'pandas==1.3.0',
+        'python-dotenv==0.19.0',
+        'scikit_learn == 0.24.2v'
+    ],
 )
