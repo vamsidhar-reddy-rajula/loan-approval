13f9ac2f5b82a83339ef47b33d762d29d5f8a6cf
vamsidhar Reddy Rajula <vamsidhar_reddy_rajula@protonmail.com>
2021-08-05 18:50:35 -0700

    trained base model

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predict whether a loan will be approved or not',
+    author='Vamsidhar',
+    license='MIT',
+)
